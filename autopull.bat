4365a6fa19a263ba553c46a30cafd277b3d235d2
FoKiRu <xkiladx@gmail.com>
2024-07-19 14:48:12 +0300

    Auto commit

--- autopull.bat
+++ autopull.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d "%USERPROFILE%\TestSsave"
 git pull origin main
+::git config --global credential.helper wincred
--- autopush.bat
+++ autopush.bat
@@ -3,3 +3,4 @@ cd /d "%USERPROFILE%\TestSsave"
 git add .
 git commit -m "Auto commit"
 git push origin main
+::git config --global credential.helper wincred
